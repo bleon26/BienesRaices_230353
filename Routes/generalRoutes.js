642ce2ec7dab6d402887b18a39c64867ff9c6f85
Brandon Leon Cabrera <230353@utxicotepec.edu.mx>
2024-11-08 10:56:25 -0600

    practica12
    practica12

--- Routes/generalRoutes.js
+++ Routes/generalRoutes.js
@@ -2,7 +2,6 @@ import express from 'express';
 
 const router = express.Router();
 
-<<<<<<< HEAD
 router.get("/", function(req, res){
     res.send("Hola Mundo desde Node, a través del Navegador");
 })
@@ -17,17 +16,3 @@ router.get("/QuienSoy", function(req, res){
 })
 
 export default router; //Esta palabra reservada de JS me permite exportar los elementos definidos y utilizarlos en otros archivos del mismo sitio. 
-=======
-router.get("/",function(req, res){
-    res.send("Hola mundo desde Node, a través del navegador")
-});
-router.get("/QuienSoy",function(req, res){
-    res.json({"Estudiante": "Brandon Leon Cabrera",
-        "grado":"4°",
-        "grupo":"B",
-        "asignatura":"Aplicaciones de Software Orientado a Servicios (AWOS)"});
-    })
-
-    export default router; //Esta palabra reservada de JS me permite exportar los elementos definidos y utilizarlos en otros dispositivos del mismo sitio
-    
->>>>>>> Brandon_practica11
